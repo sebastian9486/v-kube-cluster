0e505268500880201e1d09798ebd6e6a8164b6d2
sebastian <sommerfeld.sebastian@gmail.com>
2021-02-15 14:28:39 +0100

    added deployment

---
+++ src/main/kube-cluster/provision/deployments/dashboard.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo
+
+#
+# deploy applications
+#
+echo "[INFO] ----- application deployment: dashboard --------------------------"
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+echo "[DONE] deployed dashboard"
+
+kubectl proxy
+echo "[DONE] Created a secure channel to kubernetes cluster to access dashboard from local workstation"
+
+echo "[DONE] ----- finished application deployment: dashboard -----------------"
+echo
